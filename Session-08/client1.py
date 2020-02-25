1e93b7605003c4aa7d7c75e2c386024b4e959491
juliana1701 <j.sanmiguel.2019@alumnos.urjc.es>
2020-02-25 16:47:19 +0100

    Session 8

---
+++ Session-08/client1.py
@@ -0,0 +1,21 @@
+import socket
+
+IP = "212.128.253.128"
+PORT = 8080
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# establish the connection to the Server (IP, PORT)
+s.connect((IP, PORT))
+
+# Send data. No strings can be send, only bytes
+# It necesary to encode the string into bytes
+s.send(str.encode("HELLO FROM THE CLIENT!!!"))
+
+# Receive data from the server
+msg = s.recv(2048)
+print("MESSAGE FROM THE SERVER:\n", msg)
+print(msg.decode("utf-8"))
+
+# Closing the socket
+s.close()
