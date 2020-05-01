ff026fd77416522eea89d95f19c8a2deb3f998d4
juliana <juliana.sgonzalez17@gmail.com>
2020-05-01 19:52:45 +0200

    Final project basic level finished

--- Final-project/server.py
+++ Final-project/server.py
@@ -5,6 +5,7 @@ import termcolor
 from pathlib import Path
 import json
 
+
 def client_get_species(endpoint):
     PORT = 8080
     SERVER = 'rest.ensembl.org'
@@ -20,6 +21,19 @@ def client_get_species(endpoint):
     data = r1.read().decode("utf-8")
     data1 = json.loads(data)
     return data1
+
+
+def html_file(color, title):
+    return f"""
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title>{title}</title>
+            </head>
+            <body style="background-color: {color}">
+            </body></html>
+            """
 
 
 # Define the Server's port
@@ -52,60 +66,51 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             second_argument = arguments[1]
             third_argument = second_argument.split("=")[1]
             species = client_get_species("info/species?content-type=application/json")["species"]
+            contents = html_file("lightblue", "List of species")
             if third_argument == "":
-                contents = f"""
-                                <!DOCTYPE html>
-                                <html lang="en">
-                                <head>
-                                    <meta charset="utf-8">
-                                    <title>List of species</title>
-                                </head>
-                                <body style="background-color: lightblue">
-                                <p>Total number of species is: 267 </p>
-                                <p>The limit you have selected is:{267} </p>
-                                <p>The names of the species are:</p>
-                                </body></html>
-                                """
+                contents += f""" <p>The limit you have selected is:{267} </p>
+                                <p>The names of the species are:</p>"""
                 error_code = 200
                 for element in species:
-                    contents += f"""<p> · {element["common_name"]} </p>"""
+                    contents += f"""<p> · {element["display_name"]} </p>"""
             elif 267 >= int(third_argument):
-                contents = f"""
-                            <!DOCTYPE html>
-                            <html lang="en">
-                            <head>
-                                <meta charset="utf-8">
-                                <title>List of species</title>
-                            </head>
-                            <body style="background-color: lightblue">
-                            <p>Total number of species is: 267 </p>
-                            <p>The limit you have selected is: 267 </p>
-                            <p>The names of the species are:</p>
-                            </body></html>
-                            """
+                contents += f""" <p>Total number of species is: 267 </p>
+                            <p>The limit you have selected is: {third_argument}</p>
+                            <p>The names of the species are:</p>"""
                 error_code = 200
                 count = 0
                 for element in species:
                     if count < int(third_argument):
-                        contents += f'''<p> · {element["common_name"]}</p>'''
+                        contents += f'''<p> · {element["display_name"]}</p>'''
                     count += 1
             else:
-                contents = f"""
-                                <!DOCTYPE html>
-                                <html lang="en">
-                                <head>
-                                    <meta charset="utf-8">
-                                    <title>List of species</title>
-                                </head>
-                                <body style="background-color: lightblue">
-                                <p>Total number of species is: 267 </p>
+                contents += f"""<p>Total number of species is: 267 </p>
                                 <p>The limit you have selected is:{third_argument}</p>
-                                <p>The names of the species are:</p>
-                                </body></html>
-                                """
+                                <p>The names of the species are:</p>"""
                 error_code = 200
                 for element in species:
-                    contents += f"""<p> · {element["common_name"]} </p>"""
+                    contents += f"""<p> · {element["display_name"]} </p>"""
+        elif firts_argument == "/karyotype":
+            second_argument = arguments[1]
+            third_argument = second_argument.split("=")[1]
+            species = client_get_species("info/assembly/" + third_argument + "?content-type=application/json")["karyotype"]
+            contents = html_file("pink", "Name of chromosomes:")
+            contents += f"""<p>The names of the chromosomes are: </p>"""
+            error_code = 200
+            for element in species:
+                contents += f"""<p> · {element} </p>"""
+        elif firts_argument == "/chromosomeLength":
+            second_argument = arguments[1]
+            third_argument, fourth_argument = second_argument.split("&")
+            specie = third_argument.split("=")[1]
+            chromosome = fourth_argument.split("=")[1]
+            species = client_get_species("info/assembly/" + specie + "?content-type=application/json")["top_level_region"]
+            contents = html_file("plum", "Chromosome length")
+            for element in species:
+                if element["coord_system"] == "chromosome":
+                    if element["name"] == chromosome:
+                        contents += f"""<p> The length of the chromosome is: {element["length"]} </p>"""
+            error_code = 200
         else:
             contents = Path('Error.html').read_text()
             error_code = 404
